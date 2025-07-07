2d976cea15a6b757716c79ffd61cc60f06c079a9
Piero Toffanin <pt@masseranolabs.com>
2025-07-07 12:34:40 -0400

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'LibreTranslate',
+			social: [
+				{ icon: 'discourse', label: 'Community Forum', href: 'https://community.libretranslate.com' },
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/LibreTranslate/' },
+				{ icon: 'blueSky', label: 'BlueSky', href: 'https://bsky.app/profile/libretranslate.com' }
+			],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
